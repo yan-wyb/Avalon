4be5b5ac8a21f95dbd376c1b2d463fd4d3249e96
yan <yan-wyb@foxmail.com>
2020-06-09 14:50:07 +0800

    framework :         1 . update githooks scripts
    Signed-off-by: yan <yan-wyb@foxmail.com>

--- .githooks-build.sh
+++ .githooks-build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+git config user.email yan-wyb@foxmail.com
+git config user.name yan
 /home/yan/.rvm/gems/ruby-2.7.0/wrappers/bundle build
 /home/yan/.rvm/gems/ruby-2.7.0/wrappers/jekyll build
 
